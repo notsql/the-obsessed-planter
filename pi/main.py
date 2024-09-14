75eb7f2f012e63ec94f714a4b49ddb81428500c3
Ong Zheng Kai <zhengkai@notdns.me>
2024-09-14 23:17:21 +0800

    [feat] added code to fetch audio file and play latest 5 tracks

---
+++ pi/main.py
@@ -0,0 +1,59 @@
+from threading import Event
+import json
+
+import pygame
+
+from firebase_admin import initialize_app, firestore
+from vonage import Client
+
+TOP = 5
+playlist = []
+
+vcSecrets = json.load(open("./vonageAccountKey.json"))
+
+pygame.mixer.init()
+
+app = initialize_app()
+db = firestore.client(app)
+
+vc = Client(
+  application_id=vcSecrets["VONAGE_APPLICATION_ID"],
+  private_key=vcSecrets["VONAGE_APPLICATION_PRIVATE_KEY"],
+)
+
+def play_sound(file_path):
+  sound = pygame.mixer.Sound(file_path)
+  playing = sound.play()
+
+  while playing.get_busy():
+    pygame.time.delay(100)
+
+def add_fixed_playlist(file_path):
+  playlist.insert(0, file_path)
+
+  if len(playlist) > TOP:
+    playlist.pop()
+
+
+
+callback_done = Event()
+
+
+
+def on_snap(col_snap, changes, read_time):
+  for change in changes:
+    if change.type.name == "ADDED":
+      file_path = f"./output/{change.document.id}"
+      f = open(file_path, "wb+")
+
+      f.write(vc.voice.get_recording(change.document.to_dict()["recording_url"]))
+
+      f.close()
+
+      add_fixed_playlist(file_path)
+
+col_query = db.collection("Callers").on_snapshot(on_snap)
+
+while True:
+  for audio in playlist:
+    play_sound(audio)
